eb4726d4f86d2e21389a3359fe0082c54975f192
Jonathan Chan <jobird@Jonathans-MacBook-Pro.local>
2023-08-10 22:19:41 +0800

    add ci

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-};
+// before rum
+//npm install express
+
+// to run 
+// npm start
+
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const OS = require('os');
+const ENV = 'DEV';
+
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello from Node!';
+  res.send(msg);
+});
+
+app.get('/test', (req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello from /test Node!';
+  res.send(msg);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
+module.exports=app;
